903632695bc48ab29ad03010854cf5b33a742b44
Guy Smoilov <guy.smoilov@gmail.com>
2013-12-22 17:34:29 +0200

    Added BGM

---
+++ Assets/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton : MonoBehaviour 
+{
+	private static Singleton instance = null;
+	public static Singleton Instance
+	{
+		get
+		{
+			return (instance);
+		}
+	}
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			Singleton.instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
