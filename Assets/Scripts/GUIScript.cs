cd06deadec056a4b1a86f6f5a1c001a7ab976da5
Guy Smoilov <guy.smoilov@gmail.com>
2013-12-22 18:23:38 +0200

    Added quit function

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -5,6 +5,26 @@ public class GUIScript : MonoBehaviour {
 	
 	public Texture previewBoxTexture;
 
+	private bool paused = false;
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Esc"))
+		{
+			if (!paused)
+			{
+				paused = true;
+				Time.timeScale = 0;
+			}
+			else
+			{
+				paused = false;
+				Time.timeScale = 1;
+			}
+		}
+
+	}
+
 	void OnGUI()
 	{
 		if (previewBoxTexture != null)
@@ -17,5 +37,19 @@ public class GUIScript : MonoBehaviour {
 		}
 
 		GUI.Label(new Rect(Screen.width - 10 - Screen.width / 3, Screen.height - 50, Screen.width / 3, 20), "Score: " + BoardScript.score);
+
+		if (paused)
+		{
+			if (GUI.Button(new Rect(Screen.width * (1f/3), Screen.height / 2, Screen.width / 6, 40), "Quit"))
+			{
+				Application.Quit();
+			}
+
+			if (GUI.Button(new Rect(Screen.width * (1f/2), Screen.height / 2, Screen.width / 6, 40), "Resume"))
+			{
+				paused = false;
+				Time.timeScale = 1;
+			}
+		}
 	}
 }
